0e9dc95671114888e8759c648798e30873c1ea34
Cameron Rollheiser <cameronr@porch.com>
2015-10-26 11:41:34 -0700

    Rules, Rules, Rules
    Let's add the rules to a config repo.

---
+++ base.js
@@ -0,0 +1,34 @@
+module.exports = {
+  'parser': 'babel-eslint',             //     https://github.com/babel/babel-eslint
+                                        //     Set these to true or false, depending on your usage
+  'ecmaFeatures': {
+    'arrowFunctions': true,
+    'blockBindings': true,
+    'classes': true,
+    'defaultParams': true,
+    'destructuring': true,
+    'forOf': true,
+    'generators': false,
+    'modules': true,
+    'objectLiteralComputedProperties': true,
+    'objectLiteralDuplicateProperties': false,
+    'objectLiteralShorthandMethods': true,
+    'objectLiteralShorthandProperties': true,
+    'spread': true,
+    'superInFunctions': true,
+    'templateStrings': true
+  },
+  'extends': [
+    'eslint-config-porch/env',
+    'eslint-config-porch/rules/best-practices',
+    'eslint-config-porch/rules/possible-errors',
+    'eslint-config-porch/rules/style',
+    'eslint-config-porch/rules/variables'
+  ],
+  'rules': {
+/**
+ * Strict mode
+ */
+    'strict': 0                         // [x] http://eslint.org/docs/rules/strict
+  }
+};
---
+++ rules/react.js
@@ -0,0 +1,63 @@
+{
+  'plugins': [
+    'react'                             // [x] https://github.com/yannickcr/eslint-plugin-react
+  ],
+  'ecmaFeatures': {
+    'jsx': true
+  },
+  'rules': {
+
+/**
+ * JSX style
+ */
+    'jsx-quotes': [                     // [x] http://eslint.org/docs/rules/jsx-quotes
+      2, 'prefer-double'
+    ],
+    'react/display-name': 0,            // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
+    'react/jsx-boolean-value': [        // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
+        2, 'always'
+    ],
+    'react/jsx-quotes': [2, 'double'],  // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-quotes.md
+    'react/jsx-no-undef': 2,            // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-undef.md
+    'react/jsx-sort-props': 2,          // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md
+    'react/jsx-sort-prop-types': 2,     // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-sort-prop-types.md
+    'react/jsx-uses-react': 2,          // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-react.md
+    'react/jsx-uses-vars': 2,           // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-vars.md
+    'react/no-did-mount-set-state': [   // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-did-mount-set-state.md
+        2, 'allow-in-func'
+    ],
+    'react/no-did-update-set-state': 2, // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-did-update-set-state.md
+    'react/no-multi-comp': 2,           // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
+    'react/no-unknown-property': 2,     // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
+    'react/prop-types': 2,              // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
+    'react/react-in-jsx-scope': 2,      // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
+    'react/self-closing-comp': 2,       // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
+    'react/wrap-multilines': 2,         // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/wrap-multilines.md
+    'react/sort-comp': [2, {            // [x] https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
+      'order': [
+        'displayName',
+        'propTypes',
+        'contextTypes',
+        'childContextTypes',
+        'mixins',
+        'statics',
+        'defaultProps',
+        'constructor',
+        'getDefaultProps',
+        'getInitialState',
+        'getChildContext',
+        'componentWillMount',
+        'componentDidMount',
+        'componentWillReceiveProps',
+        'shouldComponentUpdate',
+        'componentWillUpdate',
+        'componentDidUpdate',
+        'componentWillUnmount',
+        '/^on.+$/',
+        '/^get.+$/',
+        '/^render.+$/',
+        'render'
+      ]
+    }]
+  }
+}
