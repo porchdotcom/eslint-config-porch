6a3dc60fa7082f442fab20954251d96189d7c551
Patrick Williams <patrickw@porch.com>
2017-04-12 15:24:32 -0700

    add to the import rules

--- rules/import.js
+++ rules/import.js
@@ -11,8 +11,9 @@ module.exports = {
         'import/default': 0, // this does not consider module.exports to be a default, so it breaks a bunch of our libraries
         'import/export': 2,
         'import/imports-first': 2,
-        'import/named': 0, // we sometimes modify the exported modules at runtime, which causes this to fail
+        'import/named': 2,
         'import/no-extraneous-dependencies': 2,
         'import/no-unresolved': 2
+        "import/no-commonjs": 2
     }
 };
