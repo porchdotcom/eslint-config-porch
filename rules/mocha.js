274135003fb70d88a12f8c93aa44700c34d5bb9f
Jason Nutter <jasondnutter@gmail.com>
2017-11-21 13:13:54 -0800

    Upgrade eslint to v4

--- rules/mocha.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        'mocha'
-    ],
-    rules: {
-        'mocha/no-exclusive-tests': 2,
-        'mocha/no-skipped-tests': 1 // for better or worse, some repos have lots of skipped tests
-    }
-};
